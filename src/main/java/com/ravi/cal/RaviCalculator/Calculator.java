df38471c62c660895a3584d949099ce1b7a3e3cc
saigunisetty <saiprasad.546@gmail.com>
2020-06-07 10:13:19 +0000

    main method 5

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -1,27 +1,21 @@
-
 package com.ravi.cal.RaviCalculator;
 
-import java.util.Scanner;
+public class Calculator {
 
-public class Calculator  {
+	public long first;
+	public long second;
 	
-	private long first;
-	private long second;
+	public Calculator(long first, long second){
+		this.first = first;
+		this.second=second;
+	}
 	
 	public long getFirst() {
 		return first;
 	}
 
-	public void setFirst(long first) {
-		this.first = first;
-	}
-
 	public long getSecond() {
 		return second;
-	}
-
-	public void setSecond(long second) {
-		this.second = second;
 	}
 
 	public long addFucn(long first, long second){
@@ -38,24 +32,18 @@ public class Calculator  {
 		
 		return first*second;
 	}
-	
+
+
 	public static void main(String[] args) {
+
+		long first = Long.parseLong(args[0]);
+		long second = Long.parseLong(args[1]);
 		
-		Calculator cc = new Calculator();
+		Calculator cal = new Calculator(first, second);
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter the first numeric value ");
-		long first = sc.nextLong();
-		cc.setFirst(first);
-		
-		System.out.println("Enter the second numeric value ");
-		long second = sc.nextLong();
-		cc.setSecond(second);
-		
-		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
-		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
-		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
-		
+		String output = String.format("\n*** Your Results ***\n\nFirst: %d\nSecond: %d\n\nSum : %d\nDifference : %d\nProduct : %d\n\n", cal.first, cal.second, cal.addFucn(first, second), cal.subFucn(first, second), cal.mulFucn(first, second));
+	    System.out.println("CALCULATOR DISPLAY");
+	    System.out.println(output);
 	}
+	
 }
